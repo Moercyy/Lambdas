976cf149a01ca9ecea21bee99fef4022946231a5
Moritz Maier <moritzmaier1308@gmail.com>
2019-09-24 11:06:17 +0200

    Erste Aufgabe erledigt

--- Example1/src/HalloJavaMitForEach.java
+++ Example1/src/HalloJavaMitForEach.java
@@ -3,8 +3,19 @@ import java.util.List;
 
 public class HalloJavaMitForEach {
 
-    List<Employee> employees = new ArrayList<>();
+
     public static void main(String[] args) {
-        
+        List<Employee> employees = new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Str1");
+        stringList.add("Str2");
+        stringList.add("Str3");
+        stringList.add("Str4");
+        System.out.println("Normal ausgeben");
+        for (String str : stringList) {
+            System.out.println(str);
+        }
+        System.out.println("Mit forEach ausgeben");
+        stringList.forEach (s -> System.out.println(s));
     }
 }
