82a8fc5e66f27d6e97ac4169ee16b27fe537d559
Moritz Maier <moritzmaier1308@gmail.com>
2019-09-24 12:30:34 +0200

    Bsp2 Fertig gestellt

--- Example2/src/mm/morisch/NumberTester.java
+++ Example2/src/mm/morisch/NumberTester.java
@@ -10,7 +10,7 @@ import static java.lang.StrictMath.sqrt;
 import static java.util.stream.LongStream.rangeClosed;
 
 public class NumberTester {
-    public interface NumberTest{
+    public interface NumberTest {
         boolean computeTest(int n);
     }
 
@@ -18,22 +18,24 @@ public class NumberTester {
     String FILE = "file.txt";
     Map<Integer, String> map = new HashMap<>();
     String filename;
-    public NumberTester(String filename){
+
+    public NumberTester(String filename) {
         filename = FILE;
         File file = new File(filename);
         testFile();
     }
 
-    public void setSetOddEvenTester(NumberTest oddTester){
-       oddTester = new NumberTest() {
-           @Override
-           public boolean computeTest(int n) {
-               return (n % 2) == 0;
-           }
-       };
+    public void setSetOddEvenTester(NumberTest oddTester) {
+        oddTester = new NumberTest() {
+            @Override
+            public boolean computeTest(int n) {
+                return (n % 2) == 0;
+            }
+        };
 
     }
-    public void setPrimeTester(NumberTest primeTester){
+
+    public void setPrimeTester(NumberTest primeTester) {
         primeTester = new NumberTest() {
             @Override
             public boolean computeTest(int n) {
@@ -42,18 +44,21 @@ public class NumberTester {
         };
 
     }
-    public void setPalindromeTester(NumberTest palindromeTester){
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
         palindromeTester = new NumberTest() {
             @Override
             public boolean computeTest(int n) {
                 String result = "";
-                for(int i = n+"".length()-1; i >= 0; i--)
-                    result += n+"".charAt(i);
-                return result.equals(n+"");
+                for (int i = n + "".length() - 1; i >= 0; i--)
+                    result += n + "".charAt(i);
+                return result.equals(n + "");
             }
         };
     }
-    public void testFile(){
+
+
+    public void testFile() {
 
         try (BufferedReader br = Files.newBufferedReader(Paths.get(FILE))) {
             String line;
@@ -61,14 +66,68 @@ public class NumberTester {
             String[] words;
             while ((line = br.readLine()) != null) {
                 words = line.split(seperator);
-                map.put(Integer.parseInt(words[0]), words[1]);
+                map.put(Integer.parseInt(words[0]), (words[1]));
             }
 
         } catch (IOException e) {
             System.err.format("IOException: %s%n", e);
         }
 
+        NumberTest isEven = (n) -> (n % 2) == 0;
+        NumberTest isPrime = (o) -> o > 1 && rangeClosed(2, (int) sqrt(o)).noneMatch(divisor -> o % divisor == 0);
+        NumberTest palindromeTester;
+        palindromeTester = n -> {
+            String result = "";
+            for (int i = n + "".length() - 1; i >= 0; i--)
+                result += n + "".charAt(i);
+            if (result.equals(n + "")) {
+                return true;
+            } else
+                return false;
+        };
+
+        Map<Integer, String> fMap = new HashMap<>();
+
+        for (Integer key : map.keySet()) {
+            switch (key) {
+                case 1:
+                    //System.out.println(isEven.computeTest(5));
+                    boolean num = isEven.computeTest(Integer.parseInt(map.get(key)));
+                    if (num) {
+                        map.put(key, "EVEN");
+                    } else {
+                        map.put(key, "ODD");
+                    }
+                    break;
+                case 2:
+                    boolean num2 = isPrime.computeTest(Integer.parseInt(map.get(key)));
+                    if (num2) {
+                        map.put(key, "IS PRIME");
+                    } else {
+                        map.put(key, "IS NO PRIME");
+                    }
+                    break;
+                case 3:
+                    boolean num3 = palindromeTester.computeTest(Integer.parseInt(map.get(key)));
+                    if (num3) {
+                        map.put(key, "IS PALINDROME");
+                    } else {
+                        map.put(key, "IS NO PALINDRONE");
+                    }
+                    break;
+            }
+        }
+        try {
+            FileWriter fw = new FileWriter(new File("fileanswers.txt"));
+
+            for (Integer key : map.keySet()) {
+                fw.write(key + " " + map.get(key));
+            }
+            fw.close();
 
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
