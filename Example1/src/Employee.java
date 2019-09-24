3935f96f57bd5abe1a2865db0f2d10af9eb25a96
Moritz Maier <moritzmaier1308@gmail.com>
2019-09-24 11:00:18 +0200

    Bsp1 initialisieren

---
+++ Example1/src/Employee.java
@@ -0,0 +1,34 @@
+public class Employee {
+    String firstname, name;
+    int age;
+
+    public Employee(String firstname, String name, int age) {
+        this.firstname = firstname;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ Example1/src/HalloJavaMitForEach.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavaMitForEach {
+
+    List<Employee> employees = new ArrayList<>();
+    public static void main(String[] args) {
+        
+    }
+}
